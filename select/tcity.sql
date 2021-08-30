8d233dfb5ab7f3ba8c3229622078094ccf6f5f5d
im-Lily <dmsruddlvhs2013@gmail.com>
2021-08-31 01:11:34 +0900

    조건문

--- select/tcity.sql
+++ select/tcity.sql
@@ -5,6 +5,7 @@ select region, name, area from tcity;
 
 -- 별칭 큰 따옴표("") 사용
 select name as "도시명", area as "면적(제곱Km)", popu as "인구(만명)" from tcity;
+-- 2. 
 select name as "도시", popu as "인구(만명)", region as "지역" from tcity;
 
 -- 계산값 출력
@@ -13,6 +14,7 @@ select name, area, popu, popu * 10000 / area as "인구밀도" from tcity;
 
 -- 더미데이터
 select 60 * 60 * 24 as "하루" from dual;
+-- 3.
 select 60 * 60 * 24 * 365 as "1년" from dual;
 
 -- where 작은 따옴표('') 사용
@@ -20,12 +22,32 @@ select 60 * 60 * 24 * 365 as "1년" from dual;
 select name, area from tcity where area > 1000;
 select * from tcity where name = '서울';
 select * from tcity where metro = 'y';
--- 1. 인구가 10만명 미만인 도시의 이름 출력
+-- 4. 인구가 10만명 미만인 도시의 이름 출력
 select name as "도시", popu * 10000 as "인구(명)" from tcity where popu < 10;
--- 2. 전라도에 있는 도시의 정보 출력
+-- 5. 전라도에 있는 도시의 정보 출력
 select * from tcity where region = '전라';
 
--- null
+-- 논리 연산자 : 두 개 이상의 조건 동시에 검색 시 and, or 연산자 사용
+-- and의 우선순위가 or보다 높기 때문에 괄호로 우선순위 명확히 지정하는 것을 권장
+select * from tcity where popu >= 100 and area >= 700;
+select * from tcity where region = '경기' and popu >= 50 or area >= 500;
+select * from tcity where region = '경기' and (popu >= 50 or area >= 500);
+select * from tcity where not(region = '경기');
+select * from tcity where not(region = '전라' or metro = 'y');
+
+-- like : 패턴으로 부분 문자열 검색
+select * from tcity where name like '%천%';
+select * from tcity where name not like '%천%';
+select * from tcity where trim(name) like '%천'; -- trim : 불필요한 공백 제거
+
+-- between : between 최소값 and 최대값, 시작과 끝 값 포함, 연속된 범위만 검색 가능
+select * from tcity where popu between 50 and 100;
+-- 면적 500 ~ 1000 사이의 도시 검색
+select * from tcity where area between 500 and 1000;
+
+-- in : 불연속적인 값 여러 개의 목록과 일치하는 레코드 검색
+select * from tcity where region in ('경상','전라');
+select * from tcity where region not in ('경상','전라');
 
 
 
--- select/tstaff.sql
+++ select/tstaff.sql
@@ -1,14 +1,40 @@
 SELECT * FROM tstaff;
 
 select name, salary from tstaff;
--- 직원 테이블에서 이름, 부서, 직급 필드만 출력
+-- 1. 직원 테이블에서 이름, 부서, 직급 필드만 출력
 select name, depart, grade from tstaff;
 
 -- where 
--- 3. 월급이 400만원 이상인 직원 이름 출력
+-- 6. 월급이 400만원 이상인 직원 이름 출력
 select name as "이름", salary * 10000 as "월급" from tstaff where salary >= 400 ;
 
 -- null
 -- 필드는 값이고, null은 상태이기 때문에 is null 연산자 사용
 select * from tstaff where score is null;
 select * from tstaff where score is not null;
+
+-- 7. 직원 목록에서 월급이 300 미만이면서 성취도는 60 이상인 직원 검색
+select name from tstaff where salary < 300 and score >= 60;
+-- 8. 영업부의 여직원 이름 검색
+select name from tstaff where depart = '영업부' and gender = '여';
+-- 9. 직원 목록에서 '정'씨 검색
+select * from tstaff where name like '정%';
+-- 10. 이름에 '신'자가 포함된 직원 검색
+select * from tstaff where name like '%신%';
+
+-- between
+select * from tstaff where name between '가' and '사';
+select * from tstaff where joindate between '20150101' and '20180101';
+-- 12. 월급이 200만원대인 직원 검색
+select * from tstaff where salary between 200 and 299;
+
+-- in
+-- in과 like 연산자는 같이 쓸 수 없음
+-- select * from tstaff where name like in ('이%', '안%'); 
+select * from tstaff where name like '이%' or name like '안%';
+-- 13. 총무부나 영업부에 근무하는 직원 검색
+select * from tstaff where depart in ('총무부','영업부');
+-- 14. 인사과나 영업부에 근무하는 대리 검색
+select * from tstaff where depart in ('인사과','영업부') and grade = '대리';
+-- 15. 차장급 이상의 여직원 검색
+select * from tstaff where grade in ('차장','부장','이사') and gender = '여';
