f7b32dabf298c2aa8b33ef014cc5dd47939a829e
im-Lily <dmsruddlvhs2013@gmail.com>
2021-08-31 00:07:43 +0900

    where & null

--- select/tcity.sql
+++ select/tcity.sql
@@ -3,7 +3,7 @@ SELECT * FROM tcity;
 select name, popu from tcity;
 select region, name, area from tcity;
 
--- 별칭
+-- 별칭 큰 따옴표("") 사용
 select name as "도시명", area as "면적(제곱Km)", popu as "인구(만명)" from tcity;
 select name as "도시", popu as "인구(만명)", region as "지역" from tcity;
 
@@ -13,4 +13,19 @@ select name, area, popu, popu * 10000 / area as "인구밀도" from tcity;
 
 -- 더미데이터
 select 60 * 60 * 24 as "하루" from dual;
-select 60 * 60 * 24 * 365 "1년" from dual;
+select 60 * 60 * 24 * 365 as "1년" from dual;
+
+-- where 작은 따옴표('') 사용
+-- sql 구문은 대소문자를 가리지 않지만, 필드안에 저장된 값은 대소문자 구분함
+select name, area from tcity where area > 1000;
+select * from tcity where name = '서울';
+select * from tcity where metro = 'y';
+-- 1. 인구가 10만명 미만인 도시의 이름 출력
+select name as "도시", popu * 10000 as "인구(명)" from tcity where popu < 10;
+-- 2. 전라도에 있는 도시의 정보 출력
+select * from tcity where region = '전라';
+
+-- null
+
+
+
--- select/tstaff.sql
+++ select/tstaff.sql
@@ -3,3 +3,12 @@ SELECT * FROM tstaff;
 select name, salary from tstaff;
 -- 직원 테이블에서 이름, 부서, 직급 필드만 출력
 select name, depart, grade from tstaff;
+
+-- where 
+-- 3. 월급이 400만원 이상인 직원 이름 출력
+select name as "이름", salary * 10000 as "월급" from tstaff where salary >= 400 ;
+
+-- null
+-- 필드는 값이고, null은 상태이기 때문에 is null 연산자 사용
+select * from tstaff where score is null;
+select * from tstaff where score is not null;
